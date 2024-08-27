7e0bd5ee0fdfd6ecdb90acce7f489394ea651b35
Arnau <arnauvidana95@gmail.com>
2024-08-27 11:46:40 +0200

    Queries v1.0

---
+++ mongoDBQueries.js
@@ -0,0 +1,18 @@
+const RT = db.collection('restaurants');
+RT.find({});
+RT.project({ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
+RT.project({ restaurant_id: 1, name: 1, borough: 1, cuisine: 1, _id: 0 });
+RT.project({ restaurant_id: 1, name: 1, borough: 1, "address.zipcode": 1, _id: 0 });
+RT.find({ borough: "Bronx" });
+RT.find({ borough: "Bronx" }).SetLimit(5);
+RT.find({ borough: "Bronx" }).SetLimit(5).SetSkip(5);
+RT.find({ "grades.score": { $gt: 90 } });
+RT.find({ "grades.score": { $gt: 80, $lt: 100 } });
+RT.find({ "address.coord.0": { $lt: -95.754168 } });
+RT.find({ cuisine: { $ne: "American" }, "grades.score": { $gt: 70 }, "address.coord.0": { $lt: -65.754168 } });
+RT.find({ cuisine: { $ne: "American" }, "grades.score": { $gt: 70 }, "address.coord.0": { $lt: -65.754168 } });
+RT.find({ cuisine: { $ne: "American" }, "grades.grade": "A", borough: { $ne: "Brooklyn" } }).sort({ cuisine: -1 });
+RT.find({ name: { $regex: /^Wil.*/ } }).project({ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
+RT.find({ name: { $regex: /.*ces$/ } }).project({ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
+RT.find({ name: { $regex: "Reg" } }).project({ restaurant_id: 1, name: 1, borough: 1, cuisine: 1 });
+RT.find({ borough: "Bronx", $or: [{ cuisine: "American" }, { cuisine: "Chinese" }] });
